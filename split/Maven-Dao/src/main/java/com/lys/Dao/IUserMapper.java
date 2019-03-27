e1784ac9c78f8fd6e831b18f6d99d7a0f5c5cacf
lys <1173925201@qq.com>
2019-03-27 21:24:18 +0800

    你好

--- split/Maven-Dao/src/main/java/com/lys/Dao/IUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lys.Dao;
-
-import com.lys.domian.User;
-
-public interface IUserMapper {
-
-    User findoneUser(Integer id);
-
-}
--- split/Maven-Service/src/main/java/com/lys/UserService/IUserServiceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.lys.UserService;
-
-import com.lys.domian.User;
-
-public interface IUserServiceMapper {
-
-    User findone(Integer id);
-}
